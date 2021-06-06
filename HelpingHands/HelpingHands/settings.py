6b0b766211733815b7ad63d21492bde8613e8267
Anisha Shruti A.T <anishashruti.at@gmail.com>
2021-06-06 20:48:34 +0530

    secret key exposure avoided

--- HelpingHands/HelpingHands/settings.py
+++ HelpingHands/HelpingHands/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4nm!mn7f$ix60y3q6l%u@sls71(4f)e^_w$cg2lfkvcqr*_3nv'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
