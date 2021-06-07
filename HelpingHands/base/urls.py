17fb9e178d25adc853d62b47b9fdbb9ea13cd1dc
Anisha Shruti A.T <anishashruti.at@gmail.com>
2021-06-07 21:48:45 +0530

    payment auth done

--- HelpingHands/base/urls.py
+++ HelpingHands/base/urls.py
@@ -7,5 +7,6 @@ app_name ='base'
 urlpatterns = [
     path('',views.home,name='home'),
     path('donate/',views.donate,name='donate'),
-    path('thanks/',views.thanks,name="thanks")
+    path('charge/', views.charge, name="charge"),
+    path('thanks/<str:args>/',views.thanks,name='thanks')
 ]
--- HelpingHands/base/views.py
+++ HelpingHands/base/views.py
@@ -6,16 +6,40 @@ import stripe
 import json
 # Create your views here.
 
-stripe.api_key =os.environ.get('STRIPE_KEY')
+stripe.api_key ='sk_test_51Gz20rIPDW7FrALVrHaj6LC99kLu5B24hSaFVAwRx4M5nvCNAkeE3oishiarGh6Mho92k9P42kE7A7zlppAiMNRI00z8Klep0N'
+# os.environ.get('STRIPE_KEY')
+print(stripe.api_key)
 
 def home(request):
 	return render(request, 'base/home.html')
      
 def donate(request):
+	return render(request, 'base/donate.html')
+
+def charge(request):
 	if request.method == 'POST':
-                amount=0
-	        print('Data:', request.POST)
-	return redirect(reverse('success', args=[amount]))
+		amount = int(request.POST['amount'])
+
+		customer = stripe.Customer.create(
+			email=request.POST['email'],
+			name=request.POST['name'],
+			source=request.POST['stripeToken']
+			)
+
+		charge = stripe.Charge.create(
+			customer=customer,
+			amount=amount*100,
+			currency='inr',
+			description="Donation",
+			address={
+    'line1': '510 Townsend St',
+    'postal_code': '98140',
+    'city': 'San Francisco',
+    'state': 'CA',
+    'country': 'US',
+  },
+			)
+	return redirect(reverse('base:thanks', args=[amount]))
 
 def thanks(request, args):
 	amount = args
