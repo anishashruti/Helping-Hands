cdd655b2b9c9df8d3dd966005c34239fc90231b6
Anisha Shruti A.T <anishashruti.at@gmail.com>
2021-06-09 18:21:22 +0530

    initial steps to send email done

--- HelpingHands/HelpingHands/settings.py
+++ HelpingHands/HelpingHands/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-
+STRIPE_KEY = os.environ.get('STRIPE_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
--- HelpingHands/base/views.py
+++ HelpingHands/base/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import JsonResponse
-import os
+from django.core.mail import send_mail
+from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 import stripe
 import json
 # Create your views here.
 
 stripe.api_key ='sk_test_51Gz20rIPDW7FrALVrHaj6LC99kLu5B24hSaFVAwRx4M5nvCNAkeE3oishiarGh6Mho92k9P42kE7A7zlppAiMNRI00z8Klep0N'
-# os.environ.get('STRIPE_KEY')
 print(stripe.api_key)
+
 
 def home(request):
 	return render(request, 'base/home.html')
@@ -43,12 +46,19 @@ def charge(request):
 
 		invoice = stripe.Invoice.create(
 		customer=our_customer,
-		collection_method='send_invoice',
-		days_until_due=30,
+		collection_method='charge_automatically',
+		description="Donation for Helping Hands",
 		)
-		invoice.send_invoice()
 
-		print(invoice)
+		intent= stripe.PaymentIntent.create(
+  		customer=our_customer,
+		amount=d_amount*100,
+		currency="inr",
+		receipt_email=email,
+		payment_method_types=["card"],
+		)
+
+		print(intent)
 	return redirect(reverse('base:thanks', args=[d_amount]))
 
 def thanks(request,args):
