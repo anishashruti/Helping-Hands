c2e6028969a77937081579d68c48a655eeb16960
Anisha Shruti A.T <anishashruti.at@gmail.com>
2021-06-20 19:50:43 +0530

    deployment

--- HelpingHands/settings.py
+++ HelpingHands/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     BASE_DIR/'static',
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
